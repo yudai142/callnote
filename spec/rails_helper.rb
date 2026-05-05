6a40b709906f79e40709d6d7c4ac15d2708717f4
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-05-05 14:30:35 +0900

    fix(issue-7): RSpec テスト設定を完成化 - FactoryBot と Shoulda Matchers を統合

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,15 @@ RSpec.configure do |config|
   # To enable this behaviour uncomment the line below.
   # config.infer_spec_type_from_file_location!
 
+  # Include FactoryBot syntax helpers
+  config.include FactoryBot::Syntax::Methods
+
+  # Shoulda Matchers configuration
+  Shoulda::Matchers.configure do |shoulda_matchers|
+    shoulda_matchers.integrate(test_framework: :rspec)
+    shoulda_matchers.library = :rails
+  end
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
